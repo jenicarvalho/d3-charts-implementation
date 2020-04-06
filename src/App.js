28d01775d9cdf740c52839ae7a595812b06e1b6c
Jeniffer Carvalho <jenicarvalho@live.com>
2020-04-05 23:05:40 -0300

    feita requisicao e adicionado no state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import './styles.css'
+import ChartWrapper from './components/ChartWrapper'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    data: [],
+    loading: false
+  }
+
+  componentDidMount = async () => {
+    this.setState({ loading: true })
+
+    await axios("https://v2-api.sheety.co/20cbb764b5782c3bee3e01960eea9c1b/kabum/year2011")
+      .then(result => this.setState({ data: result.data["year2011"], loading: false }))
+      .catch(error => console.log(error))
+  }
+
+  render() {
+
+    const { data, loading } = this.state
+
+    return (
+      <div>
+        <h1>Kabum Dashboard</h1>
+        {loading ? 'Carregando...' : (
+          <ChartWrapper data={data} />
+        )}
+      </div >
+    )
+  }
 }
 
 export default App;
